79c193b98f1b333278574ea890861e8ab64f9e33
ipeters123 <isaiah.peters64@gmail.com>
2023-04-01 23:09:12 +0000

    commentation

--- src/front/js/pages/about.js
+++ src/front/js/pages/about.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
-
+// The about us page //
 export const About = () => {
   const { store, actions } = useContext(Context);
 
